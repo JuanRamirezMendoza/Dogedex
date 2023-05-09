a6fc7c8565ee50587bf76581483b2b05fbc38155
JuanRamirezMendoza <ramirezsebastian72@gmail.com>
2023-05-08 20:08:07 -0500

    sign up validations

--- app/src/main/java/com/jsrm/dogedex/auth/SignUpFragment.kt
+++ app/src/main/java/com/jsrm/dogedex/auth/SignUpFragment.kt
@@ -1,6 +1,7 @@
 package com.jsrm.dogedex.auth
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,13 +11,54 @@ import com.jsrm.dogedex.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
 
+    private lateinit var binding: FragmentSignUpBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentSignUpBinding.inflate(inflater)
-        // Inflate the layout for this fragment
+        binding = FragmentSignUpBinding.inflate(inflater)
+        setUpSignUpButton()
         return binding.root
     }
 
+    private fun setUpSignUpButton() {
+        binding.signUpButton.setOnClickListener {
+            validateFields()
+        }
+    }
+
+    private fun validateFields() {
+        binding.emailInput.error = ""
+        binding.passwordInput.error = ""
+        binding.confirmPasswordInput.error = ""
+        val email = binding.emailEdit.text.toString()
+        val password = binding.passwordEdit.text.toString()
+        val passwordConfirmation = binding.confirmPasswordEdit.text.toString()
+
+        if (!isValidEmail(email)){
+            binding.emailInput.error = getString(R.string.email_is_not_valid)
+            return
+        }
+
+        if (password.isEmpty()){
+            binding.passwordInput.error = getString(R.string.password_must_not_be_empty)
+            return
+        }
+
+        if (passwordConfirmation.isEmpty()){
+            binding.confirmPasswordInput.error = getString(R.string.password_must_not_be_empty)
+            return
+        }
+
+        if (password != passwordConfirmation){
+            binding.confirmPasswordInput.error = getString(R.string.password_do_not_match)
+        }
+
+        //perform signup
+    }
+
+    private fun isValidEmail(email: String?): Boolean{
+        return !email.isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
 }
